26966600743b27b46ab62d3b6ac0cb7abe8259e4
grishuchkov <gri.d.a@ya.ru>
2023-03-22 16:58:48 +0300

    Создание полей и обвязки ResponseDTO

--- src/main/java/com/grishuchkov/resttranslatorservice/dto/ResponseDTO.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/dto/ResponseDTO.java
@@ -1,4 +1,13 @@
 package com.grishuchkov.resttranslatorservice.dto;
 
+import jdk.jfr.Category;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@NoArgsConstructor
+@Component
 public class ResponseDTO {
+    private String translatedText;
 }
