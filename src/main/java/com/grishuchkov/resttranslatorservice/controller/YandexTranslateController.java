b21c677728f8ebaedf877f4d432d15518a6eb7b2
grishuchkov <gri.d.a@ya.ru>
2023-03-23 02:03:46 +0300

    Добавлено получение IP при обращении к контроллерам.

--- src/main/java/com/grishuchkov/resttranslatorservice/controller/YandexTranslateController.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/controller/YandexTranslateController.java
@@ -4,7 +4,9 @@ import com.grishuchkov.resttranslatorservice.dto.RequestDTO;
 import com.grishuchkov.resttranslatorservice.dto.ResponseDTO;
 import com.grishuchkov.resttranslatorservice.repo.RequestRepository;
 import com.grishuchkov.resttranslatorservice.service.TranslateService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +19,14 @@ public class YandexTranslateController {
 
     private final TranslateService translateService;
     private final RequestRepository requestRepository;
+    private final HttpServletRequest request;
 
     @PostMapping("")
     public ResponseDTO getTranslateFromYandex(@RequestBody RequestDTO requestDTO){
+        String ip = request.getRemoteAddr();
+        System.out.println(ip);
+
+        //TO-DO передать IP внутрь сервиса.
         return translateService.getTranslateFromYandex(requestDTO);
     }
 
