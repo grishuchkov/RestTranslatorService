c256e90227b8715a1c70feed07eedae93e904fbe
grishuchkov <gri.d.a@ya.ru>
2023-03-22 12:30:25 +0300

    Тестовая реализация клиента на RestTemplate

---
+++ src/main/java/com/grishuchkov/resttranslatorservice/client/TestClient.java
@@ -0,0 +1,38 @@
+package com.grishuchkov.resttranslatorservice.client;
+
+import com.grishuchkov.resttranslatorservice.dto.RequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class TestClient {
+
+    public String translator(RequestDTO requestDTO){
+        String url = "https://translate.api.cloud.yandex.net/translate/v2/translate";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", "Api-Key AQVNyjKkGZaGRfXIkQVZymFY6QJVCvfCHjgB-IiI");
+
+        Map<String, String> jsonData = new HashMap<>();
+        jsonData.put("sourceLanguageCode",requestDTO.getLanguageFrom());
+        jsonData.put("targetLanguageCode",requestDTO.getLanguageTo());
+        jsonData.put("texts",requestDTO.getText());
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonData, headers);
+
+        String response = restTemplate.postForObject(url, request, String.class);
+        System.out.println(response);
+
+        return response;
+    }
+}
