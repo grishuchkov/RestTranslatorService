09ae5f5c51aa437e763f4ecc3776e6050f1131e2
grishuchkov <gri.d.a@ya.ru>
2023-03-22 23:41:05 +0300

    Минорные изменения

--- src/main/java/com/grishuchkov/resttranslatorservice/service/TranslateService.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/service/TranslateService.java
@@ -1,6 +1,5 @@
 package com.grishuchkov.resttranslatorservice.service;
 
-
 import com.grishuchkov.resttranslatorservice.client.TestClient;
 import com.grishuchkov.resttranslatorservice.dto.RequestDTO;
 import com.grishuchkov.resttranslatorservice.dto.ResponseDTO;
@@ -8,7 +7,6 @@ import com.grishuchkov.resttranslatorservice.dto.ResponseFromYandex;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +21,5 @@ public class TranslateService {
         responseDTO.setTranslatedText(responseFromYandex.getTranslations().get(0).getText());
        return responseDTO;
     }
-
-
 }
+
