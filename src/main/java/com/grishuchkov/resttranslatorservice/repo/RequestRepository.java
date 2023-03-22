57c7a9b5dc05ee17ddf3dafedfc1e7a662275679
grishuchkov <gri.d.a@ya.ru>
2023-03-23 00:24:05 +0300

    Минорный коммит

--- src/main/java/com/grishuchkov/resttranslatorservice/repo/RequestRepository.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/repo/RequestRepository.java
@@ -1,9 +1,7 @@
 package com.grishuchkov.resttranslatorservice.repo;
 
 import com.grishuchkov.resttranslatorservice.dto.RequestDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
