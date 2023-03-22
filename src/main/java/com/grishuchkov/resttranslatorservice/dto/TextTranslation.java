799db5807046805c599cb89b9e34efc1079243f3
grishuchkov <gri.d.a@ya.ru>
2023-03-22 17:03:10 +0300

    Обновление полей Translations, добавление Getter, Setter

--- src/main/java/com/grishuchkov/resttranslatorservice/dto/TextTranslation.java
+++
@@ -1,9 +0,0 @@
-package com.grishuchkov.resttranslatorservice.dto;
-
-import lombok.Data;
-
-@Data
-public class TextTranslation
-{
-    private final String text;
-}
---
+++ src/main/java/com/grishuchkov/resttranslatorservice/dto/Translations.java
@@ -0,0 +1,11 @@
+package com.grishuchkov.resttranslatorservice.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Translations
+{
+    private String text;
+}
