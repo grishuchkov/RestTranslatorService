bb6b59afa89854be8bfd75e12f13f1d21578110e
grishuchkov <gri.d.a@ya.ru>
2023-03-23 22:55:00 +0300

    Перенос конфигурационных переменных в application.yml.

--- src/main/java/com/grishuchkov/resttranslatorservice/client/YandexClient.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/client/YandexClient.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.grishuchkov.resttranslatorservice.dto.RequestToYandex;
 import com.grishuchkov.resttranslatorservice.dto.ResponseFromYandex;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -16,19 +17,25 @@ import org.springframework.web.client.RestTemplate;
 public class YandexClient {
     private final Gson gson;
 
+    @Value("${spring.yandex.url}")
+    private String URL;
+
+    @Value("${spring.yandex.token}")
+    private String TOKEN;
+
+
     public ResponseFromYandex translator(RequestToYandex requestToYandex){
 
-        String url = "https://translate.api.cloud.yandex.net/translate/v2/translate";
+        HttpHeaders headers = new HttpHeaders();
         RestTemplate restTemplate = new RestTemplate();
 
-        HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization", "Api-Key AQVNyjKkGZaGRfXIkQVZymFY6QJVCvfCHjgB-IiI");
+        headers.add("Authorization", TOKEN);
 
         String json = gson.toJson(requestToYandex);
 
         HttpEntity<String> request = new HttpEntity<>(json, headers);
 
-        return restTemplate.postForObject(url, request, ResponseFromYandex.class);
+        return restTemplate.postForObject(URL, request, ResponseFromYandex.class);
     }
 }
