ad1625a0d3aafbf1ebe8b0a0fa23266dee7acf10
grishuchkov <gri.d.a@ya.ru>
2023-03-23 23:03:32 +0300

    Финальная версия getTranslateFromYandex

--- src/main/java/com/grishuchkov/resttranslatorservice/controller/YandexTranslateController.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/controller/YandexTranslateController.java
@@ -1,16 +1,11 @@
 package com.grishuchkov.resttranslatorservice.controller;
 
-import com.grishuchkov.resttranslatorservice.dto.RequestDTO;
-import com.grishuchkov.resttranslatorservice.dto.ResponseDTO;
-import com.grishuchkov.resttranslatorservice.repo.RequestRepository;
+import com.grishuchkov.resttranslatorservice.dto.RequestFromUser;
+import com.grishuchkov.resttranslatorservice.dto.ResponseToUser;
 import com.grishuchkov.resttranslatorservice.service.TranslateService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
-
-import javax.sql.DataSource;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,16 +13,12 @@ import javax.sql.DataSource;
 public class YandexTranslateController {
 
     private final TranslateService translateService;
-    private final RequestRepository requestRepository;
 
     @PostMapping("")
-    public ResponseDTO getTranslateFromYandex(@RequestBody RequestDTO requestDTO, HttpServletRequest request){
-        String ip = request.getRemoteAddr();
-        return translateService.getTranslateFromYandex(requestDTO, ip);
-    }
+    public ResponseToUser getTranslateFromYandex(@RequestBody RequestFromUser requestFromUser, HttpServletRequest request){
 
-   @PostMapping("/db")
-    public void postToBase(@RequestBody RequestDTO requestDTO){
-        requestRepository.save(requestDTO);
-   }
+        String ip = request.getRemoteAddr();
+
+        return translateService.getTranslateFromYandex(requestFromUser, ip);
+    }
 }
