facb47ee0e23ecfe8aea5d920e47a1c7bd319485
grishuchkov <gri.d.a@ya.ru>
2023-03-23 22:55:44 +0300

    Переименование RequestDTO.java в RequrestFromUser

--- src/main/java/com/grishuchkov/resttranslatorservice/dto/RequestFromUser.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/dto/RequestFromUser.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
-public class RequestDTO {
+public class RequestFromUser {
 
     private final String text;
     private final String languageFrom;
