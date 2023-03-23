9e24c913ca353c36af2ae76e6047fc7d5cf14e43
grishuchkov <gri.d.a@ya.ru>
2023-03-23 12:05:29 +0300

    Перенос функционала из TestClient.java в YandexClient.java. TestClient.java удалён.

--- src/main/java/com/grishuchkov/resttranslatorservice/client/YandexClient.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/client/YandexClient.java
@@ -1,12 +1,34 @@
 package com.grishuchkov.resttranslatorservice.client;
 
+import com.google.gson.Gson;
+import com.grishuchkov.resttranslatorservice.dto.RequestToYandex;
+import com.grishuchkov.resttranslatorservice.dto.ResponseFromYandex;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "yandextranslate", url = "https://translate.api.cloud.yandex.net/translate/v2/translate")
-public interface YandexClient {
+@Component
+@RequiredArgsConstructor
+public class YandexClient {
+    private final Gson gson;
 
-    @PostMapping()
-    String translations();
+    public ResponseFromYandex translator(RequestToYandex requestToYandex){
+
+        String url = "https://translate.api.cloud.yandex.net/translate/v2/translate";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Authorization", "Api-Key AQVNyjKkGZaGRfXIkQVZymFY6QJVCvfCHjgB-IiI");
+
+        String json = gson.toJson(requestToYandex);
+
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+
+        return restTemplate.postForObject(url, request, ResponseFromYandex.class);
+    }
 }
