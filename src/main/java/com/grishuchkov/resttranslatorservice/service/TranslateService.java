ee6e9a84c655c96b98d88d1d79567813e61d5c4c
grishuchkov <gri.d.a@ya.ru>
2023-03-23 12:06:48 +0300

    Теперь Яндекс отдает не цельный перевод, а перевод по словам. Парсер возвращает строку, которая передается в responseDTO обратно пользователю.

--- src/main/java/com/grishuchkov/resttranslatorservice/service/TranslateService.java
+++ src/main/java/com/grishuchkov/resttranslatorservice/service/TranslateService.java
@@ -1,9 +1,11 @@
 package com.grishuchkov.resttranslatorservice.service;
 
-import com.grishuchkov.resttranslatorservice.client.TestClient;
+import com.grishuchkov.resttranslatorservice.client.YandexClient;
 import com.grishuchkov.resttranslatorservice.dto.RequestDTO;
+import com.grishuchkov.resttranslatorservice.dto.RequestToYandex;
 import com.grishuchkov.resttranslatorservice.dto.ResponseDTO;
 import com.grishuchkov.resttranslatorservice.dto.ResponseFromYandex;
+import com.grishuchkov.resttranslatorservice.utils.Parser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +13,24 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class TranslateService {
-    private final TestClient testClient;
+    private final YandexClient yandexClient;
     private final ResponseDTO responseDTO;
+    private final Parser parser;
+    private final RequestToYandex requestToYandex;
 
     public ResponseDTO getTranslateFromYandex(RequestDTO requestDTO){
 
-        ResponseFromYandex responseFromYandex = testClient.translator(requestDTO);
+        String[] wordsArray = parser.stringToList(requestDTO.getText());
 
-        responseDTO.setTranslatedText(responseFromYandex.getTranslations().get(0).getText());
-       return responseDTO;
+        requestToYandex.setSourceLanguageCode(requestDTO.getLanguageFrom());
+        requestToYandex.setTargetLanguageCode(requestDTO.getLanguageTo());
+        requestToYandex.setTexts(wordsArray);
+
+        ResponseFromYandex responseFromYandex = yandexClient.translator(requestToYandex);
+
+        responseDTO.setTranslatedText(parser.responseFromYandexToString(responseFromYandex));
+
+        return responseDTO;
     }
 }
 
